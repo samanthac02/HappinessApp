5ab70d44dba5d837ad58b9fa019a116ad83037e8
samanthac02 <72635022+samanthac02@users.noreply.github.com>
2023-01-09 00:50:01 -0800

    Add files via upload

---
+++ Current Location with GPS in SwiftUI/Current Location with GPS in SwiftUI/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Current Location with GPS in SwiftUI
+//
+//  Created by Samantha Chang on 6/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject var locationManager = LocationManager()
+    
+    var userLatitude: String {
+        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
+    }
+    
+    var userLongitude: String {
+        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+    }
+    
+    var body: some View {
+        VStack {
+            Text("location status: \(locationManager.statusString)")
+            HStack {
+                Text("latitude: \(userLatitude)")
+                Text("longitude: \(userLongitude)")
+            }
+        }
+    }
+}
---
+++ Current Location with GPS in SwiftUI/Current Location with GPS in SwiftUI/Current_Location_with_GPS_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Current_Location_with_GPS_in_SwiftUIApp.swift
+//  Current Location with GPS in SwiftUI
+//
+//  Created by Samantha Chang on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Current_Location_with_GPS_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
