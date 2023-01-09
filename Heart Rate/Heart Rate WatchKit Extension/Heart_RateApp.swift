0da7bbf73715da21ead151c11d20337a68f5b420
samanthac02 <72635022+samanthac02@users.noreply.github.com>
2023-01-09 00:53:28 -0800

    Add files via upload

---
+++ Heart Rate/Heart Rate WatchKit Extension/Heart_RateApp.swift
@@ -0,0 +1,19 @@
+//
+//  Heart_RateApp.swift
+//  Heart Rate WatchKit Extension
+//
+//  Created by Samantha Chang on 1/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Heart_RateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
